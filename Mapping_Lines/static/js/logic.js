45a93062c1999458eee52dc238a5546f27c57735
MiguelMSUB <miguel.apc@outlook.com>
2022-09-07 19:35:37 -0400

    Mapping lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,26 +1,24 @@
 // Add console.log to check to see if our code is working.
 console.log("working222");
 
-// Create the map object with a center and zoom level.
-var map = L.map("mapid", {
-    center: [34.0522, -118.2437],
-    zoom: 4
-  });
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([40.7, -94.5], 4.5);
 
-// Get data from cities.js
-let cityData = cities;
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/200000,
-    color: "yellow"
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+  // Coordinates for each point to be used in the line.
+let line = [
+  [37.6213, -122.3790], //SFO
+  [47.4435, -122.3016], //SEATTLE
+  [39.8493, -104.6738], // DEN
+  [41.9786, -87.9047], // ORD
+  [40.6417, -73.7809] //JFK
+];
 
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "red",
+  dashArray: '3,6'
+}).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
@@ -29,10 +27,8 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox/dark-v10",
+  id: "mapbox/light-v10",
   accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-
-mapbox://styles/mapbox/navigation-day-v1
