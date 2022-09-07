f0a32ccdb7a79ff7fce29cb5d7ff614e3b2af331
MiguelMSUB <miguel.apc@outlook.com>
2022-09-07 13:58:03 -0400

    Map a single point downtown LA

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -3,9 +3,17 @@ console.log("working222");
 
 // Create the map object with a center and zoom level.
 var map = L.map("mapid", {
-    center: [40.7, -94.5],
-    zoom: 4
+    center: [34.0522, -118.2437],
+    zoom: 14
   });
+
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color: "black",
+  fillColor: '#ffffa1'
+}).addTo(map)
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -13,7 +21,7 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox/streets-v11",
+  id: "mapbox/dark-v10",
   accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
