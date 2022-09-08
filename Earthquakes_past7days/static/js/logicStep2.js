b86d6d571015acb9d182966c13281a8738f21432
MiguelMSUB <miguel.apc@outlook.com>
2022-09-08 17:13:44 -0400

    Adding markers acording to earthquakes magnitude

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -1,3 +1,4 @@
+// Logic file for mapping earthquakes from GEO JSON files
 
 // Add console.log to check to see if our code is working.
 console.log("working222");
@@ -45,10 +46,45 @@ let earthquake = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_
 
 // Retrieve the earthquake GeoJSON data.
 d3.json(earthquake).then(function(data) {
-//  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
-}); 
+
+/// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+
+  // We turn each feature into a circleMarker on the map.
+  pointToLayer: function(feature, latlng) {
+              console.log(data);
+              return L.circleMarker(latlng);
+          },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo
+      }).addTo(map);
+    });
+
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: "#ffae42",
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+}
+
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+
+ 
 
 
 
