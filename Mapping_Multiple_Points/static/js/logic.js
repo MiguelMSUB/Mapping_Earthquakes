d162f93093b8b3b185a44ef66076afb58cd0e5f8
MiguelMSUB <miguel.apc@outlook.com>
2022-09-08 11:20:44 -0400

    mapping GEO JSON points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -35,4 +35,3 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-mapbox://styles/mapbox/navigation-day-v1
